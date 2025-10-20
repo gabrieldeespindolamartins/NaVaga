00f3860c70e059e0375c1443cb558530e6c2befe
GABRIEL DE ESPÍNDOLA MARTINS <gabriel_e_martins@estudante.sesisenai.org.br>
2025-10-20 12:21:08 -0300

    tentativa de tailwid

--- NaVagaWeb/src/App.jsx
+++ NaVagaWeb/src/App.jsx
@@ -1,11 +1,22 @@
+
 function App() {
 
-
   return (
-    <div>
-      <h1>Olá, Mundo</h1>
-    </div>
+    <>
+      <div>
+        <p className="text-2xl"
+        >
+          Baixe NaVaga
+        </p>
+      </div>
+      <div>
+          <img src="/NaVaga.png" className="w-64 h-auto"/>
+      </div>
+      <div>
+        <p>Bora dar um stop! Disponivel agora para Android e iOS!</p>
+      </div>
+    </>  
   );
-}
+} 
 
 export default App
