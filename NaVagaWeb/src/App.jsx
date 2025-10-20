268b9eaf2b46cab32e9328ca9dd1d96de62cfcf7
gabriel <gabriel14em@gamil.com>
2025-10-20 16:10:57 -0300

    primeiras estruturas definidas

--- NaVagaWeb/src/App.jsx
+++ NaVagaWeb/src/App.jsx
@@ -3,19 +3,25 @@ function App() {
 
   return (
     <>
-      <div>
-        <p className="text-2xl"
-        >
-          Baixe NaVaga
-        </p>
-      </div>
-      <div>
-          <img src="/NaVaga.png" className="w-64 h-auto"/>
-      </div>
-      <div>
-        <p>Bora dar um stop! Disponivel agora para Android e iOS!</p>
-      </div>
-    </>  
+      <div className="relative flex flex-col items-center justify-center mt-30">
+        <div>
+          <p className="text-6xl font-bold text-white mb-5 hover:text-gray-200 transition-colors duration-300"
+          >
+            Baixe NaVaga
+          </p>
+        </div>
+        <div>
+            <img src="/NaVaga.png" className="w-225 h-auto"/>
+        </div>
+        <div>
+          <p className="text-4xl text-white font-bold text-center hover:text-gray-200 transition-colors duration-300"
+          >
+          Bora dar um stop! Disponivel <br/> agora para Android e iOS!
+          </p>
+        </div>
+      </div> 
+      <div className="absolute top-75 left-0 w-full -z-10 border-white border-dashed border-t-60"/>
+    </>
   );
 } 
 
