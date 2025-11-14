7c8649205cd9fec20755fd1bdcec8f9c2b884fd8
gabriel <gabriel14em@gamil.com>
2025-11-14 18:17:40 -0300

    simplificando global font

--- NaVagaMobile/App.jsx
+++ NaVagaMobile/App.jsx
@@ -1,10 +1,8 @@
 //npm i react-native-web @react-navigation/native react-native-screens react-native-safe-area-context @react-navigation/native-stack
 //npx expo install expo-font @expo-google-fonts/montserrat
 
-import { useFonts, Montserrat_400Regular, Montserrat_700Bold, Montserrat_800ExtraBold } from '@expo-google-fonts/montserrat';
 import { NavigationContainer } from "@react-navigation/native"; 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { ThemeProvider } from "./src/Theme/ThemeProvider.jsx";
 
 import InitialScreen from './src/screens/InitialScreen.jsx';
 import Login from './src/screens/Login.jsx';
@@ -15,26 +13,16 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
     
-    const [fontsLoad] = useFonts({
-        Montserrat_400Regular,
-        Montserrat_700Bold,
-        Montserrat_800ExtraBold,
-    });
 
-    if (!fontsLoad) return null;
-
-    Text.defaultProps = Text.defaultProps || {};
-    Text.defaultProps.style = { fontFamily: "Montserrat_400Regular"}
+   
 
     return (
-        <ThemeProvider>
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName="Initial" screenOptions={{ headerShown: false }}>
-                    <Stack.Screen name="Initial" component={InitialScreen}/>
-                    <Stack.Screen name="Register" component={Register}/>
-                    <Stack.Screen name="Login" component={Login}/>
-                </Stack.Navigator>
-            </NavigationContainer>
-        </ThemeProvider>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Initial" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Initial" component={InitialScreen}/>
+                <Stack.Screen name="Register" component={Register}/>
+                <Stack.Screen name="Login" component={Login}/>
+            </Stack.Navigator>
+        </NavigationContainer>
     )
 }
