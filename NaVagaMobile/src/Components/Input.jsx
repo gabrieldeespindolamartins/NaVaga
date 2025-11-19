b5c54a671a657844d05761df3910f7c5d02af4ed
GABRIEL DE ESPÍNDOLA MARTINS <gabriel_e_martins@estudante.sesisenai.org.br>
2025-11-19 11:19:35 -0300

    estilizando inputs

--- NaVagaMobile/src/Components/Input.jsx
+++ NaVagaMobile/src/Components/Input.jsx
@@ -1,15 +1,38 @@
-import { TextInput, Text} from "react-native";
+import { TextInput, Text, StyleSheet} from "react-native";
 import { View } from "react-native";
 
-export default function Input({ label, placeholder, ...props }) {
+export default function InputRegister({ label, placeholder, ...props }) {
 
     return(
-        <View>
-            <Text>{label}</Text>
+        <View style={styles.container}>
+            <Text style={styles.label}>{label}</Text>
             <TextInput
+                style={styles.input}
                 placeholder={placeholder}
                 {...props}
             />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        margin: 10
+    },
+
+    label:{
+        color: '#DDE7ED',
+        fontSize: 16,
+        margin: 2
+        
+    },
+    input: {
+        paddingLeft: 20,
+        backgroundColor: '#39464E',
+        color: '#6C7A82',
+        height: 45,
+        borderRadius: 10,
+        
+    }
+});
